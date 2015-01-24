71150fd350fde80aa787bf6914254b48223d1930
Jacob Tomaw <jacob.tomaw@orbitz.com>
2015-01-24 15:56:12 -0600

    Initial Commit

---
+++ Scavenge/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Scavenge
+//
+//  Created by Jacob Tomaw on 1/24/15.
+//  Copyright (c) 2015 Jacob Tomaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
